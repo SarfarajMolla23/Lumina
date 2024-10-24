6461fafdabba6589c3674aa2cabb6a302f175f1f
SarfarajMolla23 <sarfarajmollah49@gmail.com>
2024-10-24 19:21:17 +0530

    c

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
 import postRoutes from "./routes/posts.js";
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,17 +14,14 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test";
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
     )
   )
   .catch((error) => console.log(`${error} did not connect`));
-
-mongoose.set("useFindAndModify", false);
