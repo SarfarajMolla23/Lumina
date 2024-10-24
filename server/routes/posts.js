8d35417da3f2622e3d6ff552fb3efc054eae8e4c
SarfarajMolla23 <sarfarajmollah49@gmail.com>
2024-10-24 18:51:06 +0530

    c

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,20 +1,21 @@
 import express from "express";
+
 import {
   getPosts,
+  getPost,
   createPost,
   updatePost,
-  deletePost,
   likePost,
+  deletePost,
 } from "../controllers/posts.js";
 
 const router = express.Router();
 
-// localhost:5000/posts
-
 router.get("/", getPosts);
 router.post("/", createPost);
+router.get("/:id", getPost);
 router.patch("/:id", updatePost);
 router.delete("/:id", deletePost);
-router.patch('/:id/likePost', likePost);
+router.patch("/:id/likePost", likePost);
 
 export default router;
